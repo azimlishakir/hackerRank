1c84b477e6fb8cf37f4e0a5b6f61b540fdb13738
Shakir Azimli <azimlishakir@gmail.com>
2024-03-24 12:29:55 +0400

    fix architecture repo

--- java/welcome_to_java/Solition.java
+++ java/welcome_to_java/Solition.java
@@ -1,6 +1,4 @@
-package welcome_to_java;
-
-public class Main {
+public class Solition {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
